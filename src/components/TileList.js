a39889b92ee68c01281305e34a7dd070507e0f11
Izabela Krupinska <izabela.krupinska@substrakt.com>
2023-04-06 14:27:41 +0100

    ADD: wire up create tile logic with BE and add refresh tiles functionality and fix promises when res not ok

--- src/components/TileList.js
+++ src/components/TileList.js
@@ -1,27 +1,71 @@
 import Tile from "./Tile";
 import { useEffect, useState, useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
+import Button from "react-bootstrap/Button";
 
 function TileList() {
   const { refreshTiles, tiles } = useContext(GlobalContext);
-  useEffect(() => {
+  const { addTile } = useContext(GlobalContext);
+
+  const fetchTiles = () => {
     fetch("http://localhost:8000/api/tiles/")
-      .then((response) => response.json())
-      .then((tilesDada) => refreshTiles(tilesDada))
+      .then((response) =>
+        response
+          .json()
+          .then((data) =>
+            response.ok ? refreshTiles(data) : Promise.reject(data)
+          )
+      )
       .catch((err) => {
         console.error(err.message);
       });
+  };
+  useEffect(() => {
+    fetchTiles();
   }, []);
 
+  const createNewTile = () => {
+    const tileStatus = "PENDING";
+    fetch("http://localhost:8000/api/tiles/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ status: tileStatus }),
+    })
+      .then((response) =>
+        response
+          .json()
+          .then((data) => (response.ok ? addTile(data) : Promise.reject(data)))
+      )
+      .catch((err) => {
+        console.error(err.message);
+      });
+  };
+
   return (
-    <div
-      style={{ margin: "auto", gap: "10px", width: "100%" }}
-      className="container row row-cols-3 mt-5"
-    >
-      {tiles.map((tile) => (
-        <Tile key={tile.id} tile={tile} status={tile.status} />
-      ))}
-    </div>
+    <>
+      <div className="row">
+        <Button
+          onClick={() => createNewTile()}
+          className="btn-outline-dark btn-warning"
+        >
+          ADD NEW TILE
+        </Button>
+        <Button
+          onClick={() => fetchTiles()}
+          className="btn-outline-dark btn-primary"
+        >
+          REFRESH TILES
+        </Button>
+      </div>
+      <div
+        style={{ margin: "auto", gap: "10px", width: "100%" }}
+        className="container row row-cols-3 mt-5"
+      >
+        {tiles.map((tile) => (
+          <Tile key={tile.id} tile={tile} status={tile.status} />
+        ))}
+      </div>
+    </>
   );
 }
 
